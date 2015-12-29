019d509dc2a850aef10b9a97fa6b3cea815e38a0
Zlate87 <Zlatko.Stamatov@gmail.com>
2015-12-29 20:14:31 +0100

    Initial import.

---
+++ app/src/main/java/com/zlate87/sample_transport_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zlate87.sample_transport_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
