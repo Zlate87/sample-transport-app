21eeb771bcaee0104adea01fbdc33afba0f82cda
Zlate87 <Zlatko.Stamatov@gmail.com>
2016-01-03 00:40:55 +0100

    Updates the production module.

--- app/src/production/java/com/zlate87/sample_transport_app/base/injection/ProductionModule.java
+++ app/src/production/java/com/zlate87/sample_transport_app/base/injection/ProductionModule.java
@@ -1,5 +1,8 @@
 package com.zlate87.sample_transport_app.base.injection;
 
+import android.content.Context;
+
+import com.zlate87.sample_transport_app.base.App;
 import com.zlate87.sample_transport_app.feature.routing.service.BackendRoutingService;
 import com.zlate87.sample_transport_app.feature.routing.service.RoutingService;
 
@@ -13,6 +16,25 @@ import dagger.Provides;
  */
 @Module(includes = BaseModule.class)
 public class ProductionModule {
+
+	private final App app;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param app the application context
+	 */
+	public ProductionModule(App app) {
+		this.app = app;
+	}
+
+	/**
+	 * Allow the application context to be injected but require that it be annotated with
+	 */
+	@Provides @Singleton
+	Context provideApplicationContext() {
+		return app;
+	}
 
 	/**
 	 * Dagger 2 provider method.
